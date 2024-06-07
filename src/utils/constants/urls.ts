bfebbb3a0fcf2240db41f1e4d25dcb558fb87db0
Liam Sutton <liam_sutton1990@hotmail.co.uk>
2024-06-07 21:59:35 +0100

    update urls

---
+++ src/utils/constants/urls.ts
@@ -0,0 +1,5 @@
+// const baseUrl = 'https://security.chain-detective.workers.dev';
+export const baseUrl = 'https://chain-detective-api-43f8633b2a5d.herokuapp.com';
+export const etherscanBaseUrl = 'https://etherscan.io';
+
+export default baseUrl;
