17a49c35057b7a8bad0a6d1e9624c33882774e40
Liam Sutton <liam_sutton1990@hotmail.co.uk>
2024-06-08 15:40:27 +0100

    add volume endpoint

--- src/utils/endpoints/index.ts
+++ src/utils/endpoints/index.ts
@@ -2,7 +2,9 @@ import baseUrl from '../constants/urls';
 
 const endpoints = {
   getWalletRoi: (page: number, withEthPrice: boolean) =>
-    `${baseUrl}/dailyWalletRoiInX?page=${page}&withEthPrice=${withEthPrice}`, // get
+    `${baseUrl}/dailyWalletRoiInX?page=${page}&withEthPrice=${withEthPrice}`, // GET
+  getWalletVolume: (page: number, withEthPrice: boolean) =>
+    `${baseUrl}/dailyWalletVolume?page=${page}&withEthPrice=${withEthPrice}`, // GET
 };
 
 export default endpoints;
