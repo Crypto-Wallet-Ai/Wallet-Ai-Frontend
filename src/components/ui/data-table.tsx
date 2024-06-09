8147b2af154fde6677b7b97c5d400c39bb235511
Liam Sutton <liam_sutton1990@hotmail.co.uk>
2024-06-09 21:01:19 +0100

    add sorting to data table

--- src/components/ui/data-table.tsx
+++ src/components/ui/data-table.tsx
@@ -1,11 +1,14 @@
 'use client';
 
+import { useState } from 'react';
 import {
   ColumnDef,
   flexRender,
   getCoreRowModel,
+  SortingState,
   useReactTable,
   getPaginationRowModel,
+  getSortedRowModel,
 } from '@tanstack/react-table';
 import { Table, TableBody, TableCell, TableHead, TableHeader, TableRow } from '@/components/ui/table';
 import { Button } from './button';
@@ -16,16 +19,23 @@ type DataTableProps<TData, TValue> = {
 };
 
 export function DataTable<TData, TValue>({ columns, data }: DataTableProps<TData, TValue>) {
+  const [sorting, setSorting] = useState<SortingState>([]);
+
   const table = useReactTable({
     data,
     columns,
     getCoreRowModel: getCoreRowModel(),
     getPaginationRowModel: getPaginationRowModel(),
+    onSortingChange: setSorting,
+    getSortedRowModel: getSortedRowModel(),
+    state: {
+      sorting,
+    },
   });
 
   return (
     <div>
-      <div className='border-very-dark rounded-md border'>
+      <div className='rounded-md border border-very-dark'>
         <Table>
           <TableHeader className='bg-darkest'>
             {table.getHeaderGroups().map((headerGroup) => (
