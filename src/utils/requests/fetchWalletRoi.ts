625ad02bf08d58c04408c3ce9fb5eddabbb66e1d
Liam Sutton <liam_sutton1990@hotmail.co.uk>
2024-06-10 13:51:18 +0100

    validate data from wallet roi request

--- src/utils/requests/fetchWalletRoi.ts
+++ src/utils/requests/fetchWalletRoi.ts
@@ -1,17 +1,27 @@
 import endpoints from '../endpoints';
+import { RoiResponseSchema } from '../validations/roiSchema';
+import isError from './isError';
 
 const fetchWalletRoi = async (page: number, withEthPrice = true) => {
   try {
     const res = await fetch(endpoints.getWalletRoi(page, withEthPrice));
     const data = await res.json();
 
-    const walletRoi = data?.walletRoiInX;
-    const ethPrice = data?.ethPrice?.usdPrice;
+    const validatedData = RoiResponseSchema.safeParse(data);
 
-    return { walletRoi, ethPrice };
+    if (!validatedData.success) {
+      console.log('zod error', validatedData.error);
+      return [];
+    }
+
+    return validatedData.data?.walletRoiInX;
   } catch (error) {
-    // console.log('*** getTopPairs', { error, message: error.message });
-    throw error;
+    if (isError(error)) {
+      console.log('Error fetching wallet ROI:', { error, message: error.message });
+    } else {
+      console.log('Unexpected error:', error);
+    }
+    return [];
   }
 };
 
