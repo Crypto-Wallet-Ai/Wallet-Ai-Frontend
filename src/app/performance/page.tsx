83195665f63bb2ec9af83537661a5c6440ddbf12
Liam Sutton <liam_sutton1990@hotmail.co.uk>
2024-06-10 14:25:37 +0100

    validate fetched data and type correctly

--- src/app/performance/page.tsx
+++ src/app/performance/page.tsx
@@ -3,11 +3,10 @@ import { DataTable } from '@/components/ui/data-table';
 import fetchWalletPerformance from '@/utils/requests/fetchWalletPerformance';
 import { columns } from './columns';
 import PageHeader from '@/components/PageHeader/PageHeader';
-import { WalletPerformances } from '@/utils/types/performance';
+import { type WalletPerformance } from '@/utils/validations/performanceSchema';
 
 export default async function Page() {
-  const data: WalletPerformances = await fetchWalletPerformance();
-  const { walletPerformances } = data || {};
+  const data: WalletPerformance[] = await fetchWalletPerformance();
 
   return (
     <Container>
@@ -15,7 +14,7 @@ export default async function Page() {
         title='Wallet performance'
         text='Comprehensive analysis of trading profitability and activity'
       />
-      <DataTable columns={columns} data={walletPerformances} />
+      <DataTable columns={columns} data={data} />
     </Container>
   );
 }
--- src/utils/requests/fetchWalletPerformance.ts
+++ src/utils/requests/fetchWalletPerformance.ts
@@ -1,13 +1,27 @@
 import endpoints from '../endpoints';
+import isError from '../helpers/isError';
+import { PerformanceResponseSchema } from '../validations/performanceSchema';
 
 const fetchWalletPerformance = async (page = 0, type = '1M', withEthPrice = true) => {
   try {
     const res = await fetch(endpoints.getWalletPerformance(page, type, withEthPrice));
     const data = await res.json();
 
-    return data;
+    const validatedData = PerformanceResponseSchema.safeParse(data);
+
+    if (!validatedData.success) {
+      console.log('zod error', validatedData.error);
+      return [];
+    }
+
+    return validatedData.data.walletPerformances;
   } catch (error) {
-    throw error;
+    if (isError(error)) {
+      console.log('Error fetching wallet ROI:', { error, message: error.message });
+    } else {
+      console.log('Unexpected error:', error);
+    }
+    return [];
   }
 };
 
