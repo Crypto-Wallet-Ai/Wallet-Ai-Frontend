812c5829db611acf8cf0347099ab647262a74f30
Liam Sutton <liam_sutton1990@hotmail.co.uk>
2024-06-11 14:06:40 +0100

    add pagination for performance table

--- src/app/performance/page.tsx
+++ src/app/performance/page.tsx
@@ -5,8 +5,11 @@ import { columns } from './columns';
 import PageHeader from '@/components/PageHeader/PageHeader';
 import { type WalletPerformance } from '@/utils/validations/performanceSchema';
 
-export default async function Page() {
-  const data: WalletPerformance[] = await fetchWalletPerformance();
+export default async function Page({ searchParams }: { searchParams?: Record<string, string | string[]> }) {
+  const pageParam = searchParams?.page;
+  const currentPage = Array.isArray(pageParam) ? parseInt(pageParam[0], 10) : parseInt(pageParam || '0', 10);
+
+  const data: WalletPerformance[] = await fetchWalletPerformance(currentPage);
 
   return (
     <Container>
@@ -14,7 +17,7 @@ export default async function Page() {
         title='Wallet performance'
         text='Comprehensive analysis of trading profitability and activity'
       />
-      <DataTable columns={columns} data={data} />
+      <DataTable columns={columns} data={data} currentPage={currentPage} />
     </Container>
   );
 }
