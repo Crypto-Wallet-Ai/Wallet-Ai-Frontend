65b20344cefce782cd66b9e0fc091ed7c9c835c5
Liam Sutton <liam_sutton1990@hotmail.co.uk>
2024-06-13 15:26:37 +0100

    add logo to error page

--- src/app/error.tsx
+++ src/app/error.tsx
@@ -1,7 +1,8 @@
 'use client';
 
-import { Button } from '@/components/ui/button';
+import Image from 'next/image';
 import { useEffect } from 'react';
+import { Button } from '@/components/ui/button';
 
 export default function Error({ error, reset }: { error: Error & { digest?: string }; reset: () => void }) {
   useEffect(() => {
@@ -10,6 +11,7 @@ export default function Error({ error, reset }: { error: Error & { digest?: stri
 
   return (
     <div className='min-h-not-found flex flex-col items-center justify-center gap-3 text-white'>
+      <Image className='rounded-full' width={128} height={128} alt='Logo' src='/images/wallet-ai-logo.png' />
       <h2 className='text-3xl'>Something went wrong!</h2>
       <span>{error.message}</span>
       <Button className='connect-btn' onClick={() => reset()}>
