57cb549b7673beee692478c20100024da6443203
Liam Sutton <liam_sutton1990@hotmail.co.uk>
2024-06-13 17:56:21 +0100

    render either mobile or desktop nav based on screen width

--- src/components/Navigation/Navigation.tsx
+++ src/components/Navigation/Navigation.tsx
@@ -1,19 +1,31 @@
+'use client';
+
+import { useMediaQuery } from '@/hooks/use-media-query';
 import Link from 'next/link';
+import MobileMenu from '../MobileMenu/MobileMenu';
 
 export default function Navigation() {
+  const isDesktop = useMediaQuery('(min-width: 768px)');
+
   return (
-    <nav>
-      <ul className='flex items-center gap-6 text-white'>
-        <li className='text-very-light hover:text-white hover:underline'>
-          <Link href='/'>Daily Roi</Link>
-        </li>
-        <li className='text-very-light hover:text-white hover:underline'>
-          <Link href='/daily-volume'>Daily volume</Link>
-        </li>
-        <li className='text-very-light hover:text-white hover:underline'>
-          <Link href='/performance'>Performance</Link>
-        </li>
-      </ul>
-    </nav>
+    <>
+      {isDesktop ? (
+        <nav>
+          <ul className='flex items-center gap-6 text-white'>
+            <li className='text-very-light hover:text-white hover:underline'>
+              <Link href='/'>Daily Roi</Link>
+            </li>
+            <li className='text-very-light hover:text-white hover:underline'>
+              <Link href='/daily-volume'>Daily volume</Link>
+            </li>
+            <li className='text-very-light hover:text-white hover:underline'>
+              <Link href='/performance'>Performance</Link>
+            </li>
+          </ul>
+        </nav>
+      ) : (
+        <MobileMenu />
+      )}
+    </>
   );
 }
