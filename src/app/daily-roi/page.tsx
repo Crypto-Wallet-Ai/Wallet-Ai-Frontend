6790ff204df69c00c3aa2705c8cd802c32e057e9
Liam Sutton <liam_sutton1990@hotmail.co.uk>
2024-06-13 13:09:04 +0100

    use data table wrapper for all pages

--- src/app/daily-roi/page.tsx
+++ src/app/daily-roi/page.tsx
@@ -1,20 +1,21 @@
 import fetchWalletRoi from '@/utils/requests/fetchWalletRoi';
-import { type WalletRoi } from '@/utils/validations/roiSchema';
 import { getCurrentPage } from '@/utils/helpers/searchParams';
-import { DataTable } from '@/components/ui/data-table';
 import PageHeader from '@/components/PageHeader/PageHeader';
 import Container from '@/components/Container/Container';
 import { columns } from './columns';
+import { DataTableWrapper } from '@/components/DataTableWrapper/DataTableWrapper';
 
 export default async function Page({ searchParams }: { searchParams?: Record<string, string | string[]> }) {
   const currentPage = getCurrentPage(searchParams);
 
-  const data: WalletRoi[] = await fetchWalletRoi(currentPage);
-
   return (
     <Container>
       <PageHeader title='wallet roi' text='Discover wallets with the highest ROI in the last 24 hours' />
-      <DataTable columns={columns} data={data} currentPage={currentPage} />
+      <DataTableWrapper
+        fetchData={() => fetchWalletRoi(currentPage)}
+        columns={columns}
+        currentPage={currentPage}
+      />
     </Container>
   );
 }
--- src/app/performance/page.tsx
+++ src/app/performance/page.tsx
@@ -1,15 +1,12 @@
 import Container from '@/components/Container/Container';
-import { DataTable } from '@/components/ui/data-table';
 import fetchWalletPerformance from '@/utils/requests/fetchWalletPerformance';
-import { type WalletPerformance } from '@/utils/validations/performanceSchema';
 import { getCurrentPage } from '@/utils/helpers/searchParams';
 import PageHeader from '@/components/PageHeader/PageHeader';
 import { columns } from './columns';
+import { DataTableWrapper } from '@/components/DataTableWrapper/DataTableWrapper';
 
 export default async function Page({ searchParams }: { searchParams?: Record<string, string | string[]> }) {
   const currentPage = getCurrentPage(searchParams);
-
-  const data: WalletPerformance[] = await fetchWalletPerformance(currentPage);
 
   return (
     <Container>
@@ -17,7 +14,11 @@ export default async function Page({ searchParams }: { searchParams?: Record<str
         title='Wallet performance'
         text='Comprehensive analysis of trading profitability and activity'
       />
-      <DataTable columns={columns} data={data} currentPage={currentPage} />
+      <DataTableWrapper
+        fetchData={() => fetchWalletPerformance(currentPage)}
+        columns={columns}
+        currentPage={currentPage}
+      />
     </Container>
   );
 }
