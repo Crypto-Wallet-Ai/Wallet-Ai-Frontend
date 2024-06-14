90137a558a4bac7bf473715f89c61cf38fbb9826
Liam Sutton <liam_sutton1990@hotmail.co.uk>
2024-06-14 07:40:18 +0100

    add max width to error message and center text

--- src/app/error.tsx
+++ src/app/error.tsx
@@ -10,10 +10,10 @@ export default function Error({ error, reset }: { error: Error & { digest?: stri
   }, [error]);
 
   return (
-    <div className='min-h-minus-header flex flex-col items-center justify-center gap-3 text-white'>
+    <div className='flex min-h-minus-header flex-col items-center justify-center gap-3 text-center text-white'>
       <Image className='rounded-full' width={128} height={128} alt='Logo' src='/images/wallet-ai-logo.png' />
       <h2 className='text-3xl'>Something went wrong!</h2>
-      <span>{error.message}</span>
+      <span className='max-w-[30rem] text-center'>{error.message}</span>
       <Button className='connect-btn' onClick={() => reset()}>
         Try again
       </Button>
